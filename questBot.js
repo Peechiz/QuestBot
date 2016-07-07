ceadb13a9ecd45769db3aca81a5b2155a3e5baf5
Chris <cimpicci@gmail.com>
2016-07-07 11:37:04 -0500

    adds a bunch of new quests, and adds flexibility to GetRelatedWord calls

--- questBot.js
+++ questBot.js
@@ -3,6 +3,7 @@ console.log('The QuestGiverNPC is alive');
 var Twit = require('twit');
 var rp = require('request-promise');
 require('dotenv').load();
+require('locus');
 
 var wordnikApi = process.env.WORDNIK_API_KEY;
 
@@ -36,7 +37,13 @@ function tweetQuest(quest) {
 
 // ------------------------------------------------------------------------ //
 
-var locationRoots = ['plateau', 'swamp', 'forest', 'cavern', 'palace'];
+var locations = ['plateau', 'swamp', 'forest', 'cavern', 'palace', 'sewer'];
+var occupations = ['king','duke', 'witch', 'blacksmith', 'tyrant', 'vagabond', 'warrior'];
+var loot = ['head','arm','sword','soul','amulet','goblet','sigil','shield','hide'];
+var animal = ['yak','lion','dragon','elephant','gorilla','snake','rat'];
+
+var names = ['Abrielle ' ,'Adair' ,'Adara' ,'Adriel' ,'Aiyana' ,'Alissa' ,'Alixandra' ,'Altair' ,'Amara' ,'Anatola' ,'Anya' ,'Arcadia' ,'Ariadne' ,'Arianwen' ,'Aurelia' ,'Aurelian' ,'Aurelius' ,'Avalon' ,'Acalia' ,'Alaire' ,'Auristela' ,'Bastian' ,'Breena' ,'Brielle' ,'Briallan' ,'Briseis' ,'Cambria' ,'Cara' ,'Carys' ,'Caspian' ,'Cassia' ,'Cassiel' ,'Cassiopeia' ,'Cassius' ,'Chaniel' ,'Cora' ,'Corbin' ,'Cyprian' ,'Daire' ,'Darius' ,'Destin' ,'Drake' ,'Drystan' ,'Dagen' ,'Devlin' ,'Devlyn' ,'Eira' ,'Eirian' ,'Elysia' ,'Eoin' ,'Evadne' ,'Eliron' ,'Evanth' ,'Fineas' ,'Finian' ,'Fyodor' ,'Gareth' ,'Gavriel' ,'Griffin' ,'Guinevere' ,'Gaerwn' ,'Ginerva' ,'Hadriel' ,'Hannelore' ,'Hermione' ,'Hesperos' ,'Iagan' ,'Ianthe' ,'Ignacia' ,'Ignatius' ,'Iseult' ,'Isolde' ,'Jessalyn' ,'Kara' ,'Kerensa' ,'Korbin' ,'Kyler' ,'Kyra' ,'Katriel' ,'Kyrielle' ,'Leala' ,'Leila' ,'Lilith' ,'Liora' ,'Lucien' ,'Lyra' ,'Leira' ,'Liriene' ,'Liron' ,'Maia' ,'Marius' ,'Mathieu' ,'Mireille' ,'Mireya' ,'Maylea' ,'Meira' ,'Natania' ,'Nerys' ,'Nuriel' ,'Nyssa' ,'Neirin' ,'Nyfain' ,'Oisin' ,'Oralie' ,'Orion' ,'Orpheus' ,'Ozara' ,'Oleisa' ,'Orinthea' ,'Peregrine' ,'Persephone' ,'Perseus' ,'Petronela' ,'Phelan' ,'Pryderi' ,'Pyralia' ,'Pyralis' ,'Qadira' ,'Quintessa' ,'Quinevere' ,'Raisa' ,'Remus' ,'Rhyan' ,'Rhydderch' ,'Riona' ,'Renfrew' ,'Saoirse' ,'Sarai' ,'Sebastian' ,'Seraphim' ,'Seraphina' ,'Sirius' ,'Sorcha' ,'Saira' ,'Sarielle' ,'Serian' ,'Séverin' ,'Tavish' ,'Tearlach' ,'Terra' ,'Thalia' ,'Thaniel' ,'Theia' ,'Torian' ,'Torin' ,'Tressa' ,'Tristana' ,'Uriela' ,'Urien' ,'Ulyssia' ,'Vanora' ,'Vespera' ,'Vasilis' ,'Xanthus' ,'Xara' ,'Xylia' ,'Yadira' ,'Yseult' ,'Yakira' ,'Yeira' ,'Yeriel' ,'Yestin' ,'Zaira' ,'Zephyr' ,'Zora' ,'Zorion' ,'Zaniel' ,'Zarek'];
+
 
 function randomItem(arr) {
   var index = Math.floor(Math.random()*arr.length);
@@ -82,44 +89,66 @@ var getNounPl = {
   json: true // Automatically parses the JSON string in the response
 }
 
-var getLocation = {
-  uri: `http://api.wordnik.com:80/v4/word.json/${randomItem(locationRoots)}/relatedWords`,
-  qs: {
-    useCanonical: false,
-    relationshipTypes: 'same-context',
-    limitPerRelationshipType: 30,
-    api_key: wordnikApi
-  },
-  headers: {
-    'User-Agent': 'Request-Promise'
-  },
-  json: true // Automatically parses the JSON string in the response
+function getRelated(wordArr) {
+  return {
+    uri: `http://api.wordnik.com:80/v4/word.json/${randomItem(wordArr)}/relatedWords`,
+    qs: {
+      useCanonical: false,
+      relationshipTypes: 'same-context',
+      limitPerRelationshipType: 30,
+      api_key: wordnikApi
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+  }
 }
 
 function quest(){
   var adj1 = rp(getAdj),
   adj2 = rp(getAdj),
   nounPl = rp(getNounPl),
-  location = rp(getLocation);
+  location = rp(getRelated(locations)),
+  occupation = rp(getRelated(occupations)),
+  prize = rp(getRelated(loot)),
+  beast = rp(getRelated(animal));
 
-  Promise.all([adj1,adj2,nounPl,location])
+  Promise.all([adj1,adj2,nounPl,location, occupation,prize, beast])
   .then(values => {
+
     var adj1 = values[0][0].word,
-    adj2 = values[1][0].word,
-    nounPl = values[2].word,
-    location = randomItem(values[3][0].words);
+        adj2 = values[1][0].word,
+        nounPl = values[2].word,
+        name = randomItem(names),
+        location = randomItem(values[3][0].words),
+        occupation = randomItem(values[4][0].words),
+        prize = randomItem(values[5][0].words),
+        beast = randomItem(values[6][0].words);
+
 
     var fetch = [
       `Bring me ${randomNumber()} ${adj1} ${nounPl} from the ${adj2} ${location}!`,
       `Hero! Can you get me ${randomNumber()} ${adj1} ${nounPl}? I am far too ${adj2}.`,
-      `Alas! Without ${randomNumber()} ${nounPl}, our village is doomed!`
+      `Alas! Without ${randomNumber()} ${nounPl}, our village is doomed!`,
+      `Our village can have no peace until ${name} the ${adj1} ${occupation} is destroyed!`,
+      `Bring me the ${prize} of ${name} the ${occupation}!`,
+      `Hunt ${randomNumber()} ${adj1} ${location} ${beast}, and your reward will be great`,
+      `The festival cannot commence without ${randomNumber()} ${adj1} ${nounPl}.`,
+      `You there, ${occupation}! Make yourself useful and kill these ${location} ${beast}`
     ]
 
     var quest = randomItem(fetch);
+    // eval(locus)
     tweetQuest(quest);
+    // console.log(quest);
   })
 }
 
+// rp(getRelated(locations)).then(res=>{console.log('loc:',randomItem(res[0].words))})
+// rp(getRelated(occupations)).then(res=>{console.log('job:',randomItem(res[0].words))})
+// rp(getRelated(loot)).then(res=>{console.log('loot:',randomItem(res[0].words))})
+// rp(getRelated(animal)).then(res=>{console.log('beast:',randomItem(res[0].words))})
 
 // ------------------------------------------------------------------------ //
 quest()
